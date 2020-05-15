3ceb067657f6a53458f12bbe4ae6ddd891e2f8b1
JUAN <JUAN@JUAN-PC>
2020-05-15 11:36:15 -0500

    Arreglo de categorías agregadas a la clase Categorías

--- Ahorcado/src/ahorcado/Categorias.java
+++ Ahorcado/src/ahorcado/Categorias.java
@@ -3,9 +3,20 @@ package ahorcado;
 public class Categorias {
 
     String[] colores = {"color", "blanco", "negro", "gris", "rojo", "naranja", "anaranjado", "amarillo",
-        "verde", "celeste", "azul", "violeta", "rosa", "rosado", "marrón", "café"
+        "verde", "celeste", "azul", "violeta", "rosa", "rosado", "marron", "cafe"
     };
-    
+    String[] animales={"perro","gato","caballo","morsa","nutria","oveja","mono","raton","conejo","jirafa",
+        "gallina","tigre","conejo","cerdo","pulpo","cuervo"
+    };
+    String[] tiempo={"dia","hora","minuto","segundo","mes","epoca","era","fecha","instante","calendario","edad",
+            "momento","semana","decada","siglo","milenio"
+    };
+    String[] biomas={"campo","bosque","selva","desierto","plya","rio","lago","laguna","monte","oceano",
+        "mar","cerro","meseta","llanura","costa","montaña"
+    };
+    String[] frutas={"ciruela","manzana","naranja","palta","durazno","sandia","tomate","pera","mango","platano",
+        "limon","papaya","guayaba","mandarina","fresa","cereza"        
+    };
     public void TodasLasCategorías(){
         
     }
